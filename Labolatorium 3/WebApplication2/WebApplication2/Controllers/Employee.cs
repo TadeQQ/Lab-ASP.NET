3ebe672ad1f546d022d543cd0bd54c8dbb5f824e
Teddy <88510018+TadeQQ@users.noreply.github.com>
2023-10-28 17:32:24 +0200

    feat: add some files

--- Labolatorium 3/WebApplication2/WebApplication2/Controllers/Employee.cs
+++ Labolatorium 3/WebApplication2/WebApplication2/Controllers/Employee.cs
@@ -4,23 +4,20 @@ using WebApplication2.Models;
 
 public class EmployeeController : Controller
 {
-    // Metoda akcji do wyświetlania formularza
     public IActionResult Create()
     {
         return View();
     }
 
-    // Metoda akcji do odbierania formularza
     [HttpPost]
     public IActionResult Create(Employee employee)
     {
         if (ModelState.IsValid)
         {
-            // Zapisz dane pracownika...
-            return RedirectToAction("Index"); // Przekieruj do strony głównej po zapisaniu danych
+            return RedirectToAction("Index");
         }
 
-        return View(employee); // Wróć do widoku z błędami walidacji
+        return View(employee);
     }
 }
 
