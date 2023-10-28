da3c661484997e442d3146e71004c340fc3fc00d
Teddy <88510018+TadeQQ@users.noreply.github.com>
2023-10-28 17:18:38 +0200

    feat: add Lab3 folde with project

---
+++ Labolatorium 3/WebApplication2/WebApplication2/Controllers/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApplication2.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+public class EmployeeController : Controller
+{
+    // Metoda akcji do wyświetlania formularza
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // Metoda akcji do odbierania formularza
+    [HttpPost]
+    public IActionResult Create(Employee employee)
+    {
+        if (ModelState.IsValid)
+        {
+            // Zapisz dane pracownika...
+            return RedirectToAction("Index"); // Przekieruj do strony głównej po zapisaniu danych
+        }
+
+        return View(employee); // Wróć do widoku z błędami walidacji
+    }
+}
+
---
+++ Labolatorium 3/WebApplication2/WebApplication2/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApplication2.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+public class Employee
+{
+    [HiddenInput]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać imię.")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać nazwisko.")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać PESEL.")]
+    [StringLength(11, ErrorMessage = "PESEL musi mieć 11 cyfr.")]
+    public string PESEL { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać stanowisko.")]
+    public string Position { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać oddział.")]
+    public string Department { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać datę zatrudnienia.")]
+    public DateTime EmploymentDate { get; set; }
+
+    public DateTime? TerminationDate { get; set; }
+};
+
