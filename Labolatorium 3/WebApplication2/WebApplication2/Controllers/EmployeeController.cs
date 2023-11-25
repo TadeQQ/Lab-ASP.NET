a4de86d3850b2106a3c27617e93e2eaad9381964
Teddy <88510018+TadeQQ@users.noreply.github.com>
2023-11-25 16:10:28 +0100

    feat: inject serivce to controller

--- Labolatorium 3/WebApplication2/WebApplication2/Controllers/EmployeeController.cs
+++ Labolatorium 3/WebApplication2/WebApplication2/Controllers/EmployeeController.cs
@@ -4,27 +4,90 @@ using WebApplication2.Models;
 
 public class EmployeeController : Controller
 {
+    private readonly IEmployeesService _employeeService;
+
+    public EmployeeController(IEmployeesService contactService)
+    {
+        _employeeService = contactService;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_employeeService.FindAll());
+    }
+
+    [HttpGet]
     public IActionResult CreateEmployee()
     {
         return View();
     }
 
     [HttpPost]
-    public IActionResult CreateEmployee(Employee employee)
+    public IActionResult CreateEmployee(Employee model)
     {
         if (ModelState.IsValid)
         {
+            _employeeService.Add(model);
             return RedirectToAction("Index");
         }
-
+        else
+        {
+            return View(model);
+        }
+    }
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var employee = _employeeService.FindById(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
         return View(employee);
     }
 
-    public IActionResult Index()
+    [HttpPost]
+    public IActionResult Edit(Employee model)
     {
-        List<Employee> employees = new List<Employee>(); // Pobierz listę pracowników
-        return View(employees);
+        if (ModelState.IsValid)
+        {
+            _employeeService.Update(model);
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(model);
+        }
     }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var employee = _employeeService.FindById(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        return View(employee);
+    }
+
+    [HttpPost]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        _employeeService.Delete(id);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+        var employee = _employeeService.FindById(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        return View(employee);
+    }
+
 
 }
 
