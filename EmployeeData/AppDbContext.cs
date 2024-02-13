defbb79478f96b5b2e9f85853554d089e8306ed8
TadeQ <88510018+TadeQQ@users.noreply.github.com>
2024-02-13 16:57:28 +0100

    feat: change admin and user

--- EmployeeData/AppDbContext.cs
+++ EmployeeData/AppDbContext.cs
@@ -10,14 +10,12 @@ namespace Lab3___zadanieContextConnection
         public DbSet<EmployeeEntity> Employees { get; set; }
 
         private string DbPath { get; set; }
-
         public AppDbContext()
         {
             var folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
             DbPath = System.IO.Path.Join(path, "employees.db");
         }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
@@ -29,40 +27,23 @@ namespace Lab3___zadanieContextConnection
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            string ADMIN_ID = Guid.NewGuid().ToString();
-            string USER_ROLE_ID = Guid.NewGuid().ToString();
-            string ROLE_ID = Guid.NewGuid().ToString();
 
-            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            string ADMIN_ID = Guid.NewGuid().ToString();
+            string USER_ID = Guid.NewGuid().ToString();
+            string ADMIN_ROLE_ID = Guid.NewGuid().ToString();
+            string USER_ROLE_ID = Guid.NewGuid().ToString();
+
+
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole()
             {
                 Name = "admin",
                 NormalizedName = "ADMIN",
-                Id = ROLE_ID,
-                ConcurrencyStamp = ROLE_ID
+                Id = ADMIN_ROLE_ID,
+                ConcurrencyStamp = ADMIN_ROLE_ID
             });
 
-            var admin = new IdentityUser
-            {
-                Id = ADMIN_ID,
-                Email = "adam@wsei.edu.pl",
-                EmailConfirmed = true,
-                UserName = "adam",
-                NormalizedUserName = "ADMIN"
-            };
-
-            PasswordHasher<IdentityUser> ph = new PasswordHasher<IdentityUser>();
-            admin.PasswordHash = ph.HashPassword(admin, "1234abcd!@#$ABCD");
-
-            modelBuilder.Entity<IdentityUser>().HasData(admin);
-
-            modelBuilder.Entity<IdentityUserRole<string>>()
-            .HasData(new IdentityUserRole<string>
-            {
-                RoleId = ROLE_ID,
-                UserId = ADMIN_ID
-            });
-
-            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole()
             {
                 Name = "user",
                 NormalizedName = "USER",
@@ -70,31 +51,50 @@ namespace Lab3___zadanieContextConnection
                 ConcurrencyStamp = USER_ROLE_ID
             });
 
-            string USER_ID = Guid.NewGuid().ToString();
+            var admin = new IdentityUser
+            {
+                Id = ADMIN_ID,
+                Email = "tadeusz@employees.pl",
+                NormalizedEmail = "TADEUSZ@EMPLOYEES.PL",
+                EmailConfirmed = true,
+                UserName = "Tadeusz",
+                NormalizedUserName = "TADEUSZ"
+            };
+
             var user = new IdentityUser
             {
                 Id = USER_ID,
-                Email = "user@example.com",
+                Email = "adam@employees.pl",
+                NormalizedEmail = "ADAM@EMPLOYEES.PL",
                 EmailConfirmed = true,
-                UserName = "user",
-                NormalizedUserName = "USER"
+                UserName = "Adam",
+                NormalizedUserName = "ADAM"
             };
 
-            PasswordHasher<IdentityUser> ph1 = new PasswordHasher<IdentityUser>();
-            user.PasswordHash = ph1.HashPassword(user, "user123");
+            PasswordHasher<IdentityUser> passHasher = new PasswordHasher<IdentityUser>();
+            admin.PasswordHash = passHasher.HashPassword(admin, "Haslo23!");
+            user.PasswordHash = passHasher.HashPassword(user, "Haselko23!");
 
+            modelBuilder.Entity<IdentityUser>().HasData(admin);
             modelBuilder.Entity<IdentityUser>().HasData(user);
 
+
             modelBuilder.Entity<IdentityUserRole<string>>()
-                .HasData(new IdentityUserRole<string>
+                .HasData(new IdentityUserRole<string>()
+                {
+                    RoleId = ADMIN_ROLE_ID,
+                    UserId = ADMIN_ID
+                });
+            modelBuilder.Entity<IdentityUserRole<string>>()
+                .HasData(new IdentityUserRole<string>()
                 {
                     RoleId = USER_ROLE_ID,
                     UserId = USER_ID
                 });
             
             modelBuilder.Entity<EmployeeEntity>().HasData(
-                new EmployeeEntity() { Id = 1, FirstName = "Adnrzej", LastName = "Kowalski", PESEL = "12345678910", Department = "XYS", Position = "pozycja", EmploymentDate = new DateTime(2020, 1, 1) },
-                new EmployeeEntity() { Id = 2, FirstName = "LKia", LastName = "ASdasd", PESEL = "12352635213", Department = "XYS", Position = "pozycja", EmploymentDate = new DateTime(2020, 1, 1) }
+                new EmployeeEntity() { Id = 1, FirstName = "Andrzej", LastName = "Kowalski", PESEL = "12345678910", Department = "IT", Position = "Java Developer", EmploymentDate = new DateTime(2020, 1, 1) },
+                new EmployeeEntity() { Id = 2, FirstName = "Adam", LastName = "Nowacki", PESEL = "12352635213", Department = "HR", Position = "Intern", EmploymentDate = new DateTime(2020, 1, 1) }
             );
         }
     }
