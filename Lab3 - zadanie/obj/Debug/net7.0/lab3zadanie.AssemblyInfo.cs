7f8a82bcb52a2469e42cac72e39ebc7e2f933777
TadeQ <88510018+TadeQQ@users.noreply.github.com>
2024-02-13 17:26:48 +0100

    feat: update files

--- Lab3 - zadanie/obj/Debug/net7.0/lab3zadanie.AssemblyInfo.cs
+++ Lab3 - zadanie/obj/Debug/net7.0/lab3zadanie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab3zadanie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a3c2fc3aadd35019ff6cc6c2d3ba99b7f7909f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e376e105e9e41f06f2133edd909c05819ebe07")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab3zadanie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab3zadanie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
