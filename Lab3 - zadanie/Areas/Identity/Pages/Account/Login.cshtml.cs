5a70edf12897aa144b519b979b3d32c018240417
TadeQ <88510018+TadeQQ@users.noreply.github.com>
2024-02-13 16:59:43 +0100

    feat: add auth for admin or user

--- Lab3 - zadanie/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Lab3 - zadanie/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+﻿﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
@@ -67,7 +67,8 @@ namespace Lab3___zadanie.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            public string UserName { get; set; }
+            [EmailAddress]
+            public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -112,7 +113,8 @@ namespace Lab3___zadanie.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var user = await _userManager.FindByEmailAsync(Input.Email);
+                var result = await _signInManager.PasswordSignInAsync(user?.UserName ?? "", Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
